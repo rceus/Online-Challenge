d29d0280caa2bf26ee5b84f988b9723ddee28970
rishabhjain <rishabhjain2795@gmail.com>
2014-07-14 17:10:48 +0000

    working code

--- product_all_but_one.py
+++ product_all_but_one.py
@@ -1,9 +1,29 @@
 def main():
 	product = 1
-	length_array = int(raw_input())
-	num_zero = 0
+	length = int(input())
+	zeroes = 0
 	arr = []
 
-	for i in range(length_array):
-		length_array = int(raw_input())
-		if num:
+	for i in range(length):
+		number = int(input())
+		if number==0:
+			zeroes +=1
+		else:
+			product *= number
+		arr.append(number)
+
+	if zeroes > 1:
+		for i in range(len(arr)):
+			print(0)
+	elif zeroes == 0:
+		for j in xrange(len(arr)):
+			print(product//arr[j])
+	else: #zeroes are 1
+		for k in xrange(len(arr)):
+			if arr[k] == 0:
+				print(product)
+			else:
+				print(0)
+
+if __name__ == '__main__':
+	main()
